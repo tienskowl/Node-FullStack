c7e092d0f3792e2d31880ae799760339a6e29aa9
康康 <kngkngtian@gmail.com>
2020-03-21 00:09:22 +0800

    服务端登陆验证 完成

--- admin/src/http.js
+++ admin/src/http.js
@@ -2,10 +2,26 @@ import axios from 'axios';
 
 import Vue from 'vue';
 
+import router from './router/index';
+
 const http = axios.create({
   baseURL: 'http://localhost:3000/admin/api'
 });
 
+// 拦截请求体添加token参数
+http.interceptors.request.use(
+  config => {
+    if (localStorage.token) {
+      config.headers.Authorization = 'Bearer ' + localStorage.token;
+    }
+    return config;
+  },
+  err => {
+    return Promise.reject(err);
+  }
+);
+
+// 拦截返回体
 http.interceptors.response.use(
   res => {
     return res;
@@ -17,6 +33,11 @@ http.interceptors.response.use(
         type: 'error',
         message: err.response.data.message
       });
+    }
+    if (err.response.status === 401) {
+      // 这样不行，访问不到
+      // Vue.$router.push('/login');
+      router.push('/login');
     }
   }
 );
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,22 +1,40 @@
 module.exports = app => {
   const express = require('express');
   const router = express.Router();
-  // 托管静态文件
+  const jwt = require('jsonwebtoken');
+  const AdminUser = require('../../models/AdminUser');
+  const assert = require('http-assert');
   router.post('/', async (req, res) => {
     const model = await req.Model.create(req.body);
     res.send(model);
   });
-  // 返回数据时将父级分类也返回
-  router.get('/', async (req, res) => {
-    let queryOptions = {};
-    if (req.Model.modelName === 'Category') {
-      queryOptions.populate = 'parent';
+  // 返回数据时将父级分类也返回  // 此处添加验证
+  router.get(
+    '/',
+    async (req, res, next) => {
+      const token = String(req.headers.authorization || '')
+        .split(' ')
+        .pop();
+      console.log(token);
+      assert(token, 401, '请先登录');
+      const { id } = jwt.verify(token, app.get('secret'));
+      assert(id, 401, '请先登录');
+      req.user = AdminUser.findById(id);
+      assert(req.user, 401, '请先登录');
+
+      await next();
+    },
+    async (req, res) => {
+      let queryOptions = {};
+      if (req.Model.modelName === 'Category') {
+        queryOptions.populate = 'parent';
+      }
+      const items = await req.Model.find()
+        .setOptions(queryOptions)
+        .limit(10);
+      res.send(items);
     }
-    const items = await req.Model.find()
-      .setOptions(queryOptions)
-      .limit(10);
-    res.send(items);
-  });
+  );
   router.get('/:id', async (req, res) => {
     const model = await req.Model.findById(req.params.id);
     res.send(model);
@@ -60,25 +78,38 @@ module.exports = app => {
     // 根据用户名找用户
     const User = require('../../models/AdminUser');
     // 向数据库查询需要await
-    const model = await User.findOne({ username: username }).select(
+    const user = await User.findOne({ username: username }).select(
       '+password'
       // 标识要取出password
     );
-    if (!model) {
-      return res.status(422).send({
-        message: '用户不存在'
-      });
-    }
+    // if (!user) {
+    //   return res.status(422).send({
+    //     message: '用户不存在'
+    //   });
+    // }
+    // 使用assert替换上述
+    assert(user, 422, '用户不存在');
     // 校验密码
-    const isValid = require('bcrypt').compareSync(password, model.password);
-    if (!isValid) {
-      return res.status(422).send({
-        message: '密码错误'
-      });
-    }
+    const isValid = require('bcrypt').compareSync(password, user.password);
+    // if (!isValid) {
+    //   return res.status(422).send({
+    //     message: '密码错误'
+    //   });
+    // }
+    // 使用assert替换上述
+    assert(isValid, 422, '密码错误');
     // 返回token  使用 jsonwebtoken 验证token
     const jwt = require('jsonwebtoken');
-    const token = jwt.sign({ id: model._id }, app.get('secret'));
+    const token = jwt.sign({ id: user._id }, app.get('secret'));
     res.send({ token });
+  });
+
+  // http-assert对应的错误处理中间件
+  app.use((error, req, res, next) => {
+    //发送使用assert语句传入的状态码和错误提示信息
+    // 因为所有错误都被捕获，有的没有错误码，要有默认值
+    res.status(error.statusCode || 500).send({
+      message: error.message
+    });
   });
 };
